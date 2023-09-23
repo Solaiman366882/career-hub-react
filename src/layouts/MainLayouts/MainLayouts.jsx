423ec9ccbebad7e2c66d0b41b66577b76791dced
Md.Solaiman <md.solaiman366882@gmail.com>
2023-09-24 01:07:11 +0600

    Footer component added

--- src/layouts/MainLayouts/MainLayouts.jsx
+++ src/layouts/MainLayouts/MainLayouts.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 
 
 const MainLayouts = () => {
     return (
-        <div className=" p-20 bg-rose-400 min-h-screen w-full">
-            <Outlet></Outlet>
+        <div className="flex flex-col justify-between bg-rose-400 min-h-screen w-full">
+            <main className=" max-w-screen-xl mx-auto">
+                <Outlet></Outlet>
+            </main>
+            <Footer></Footer>
         </div>
     );
 };
