d021a4ebd46cb10cc68f645c8998da0ad8ea6e7c
Md.Solaiman <md.solaiman366882@gmail.com>
2023-09-23 21:52:36 +0600

    tailwind css downloaded and important files and folder uploaded for react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-3xl text-rose-600 text-center py-20'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
